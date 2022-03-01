c6b81c95b707f0957013cc3a92c06411635946fd
Iogann1978 <iogann1978@gmail.com>
2022-03-01 22:57:52 +0300

    rework descript controller

--- src/main/java/ru/home/itinfo/controller/DescriptController.java
+++ src/main/java/ru/home/itinfo/controller/DescriptController.java
@@ -4,13 +4,18 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.home.itinfo.service.DescriptService;
+
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/descript")
 @Tag(name = "DescriptController", description = "Контроллер работы с описанием")
+@Slf4j
 public class DescriptController {
     private DescriptService descriptService;
 
@@ -30,5 +35,20 @@ public class DescriptController {
             @PathVariable Long courseId
     ) {
         return descriptService.getFromCourse(courseId);
+    }
+
+    @PostMapping("/{id}")
+    @Operation(summary = "Обновить описание")
+    public void descriptUpload(
+            @Parameter(description = "id описания")
+            @PathVariable Long id,
+            @Parameter(description = "Содержимое файла")
+            @RequestParam("file") MultipartFile file
+    ) {
+        try {
+            log.info("file: {} {}", id, new String(file.getBytes()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
