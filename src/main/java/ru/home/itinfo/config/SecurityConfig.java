fc4c84eb28b6921c6a9755784ba52f02fcf6e887
Iogann1978 <iogann1978@gmail.com>
2022-03-04 23:35:15 +0300

    cord and csrf

--- src/main/java/ru/home/itinfo/config/SecurityConfig.java
+++ src/main/java/ru/home/itinfo/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity security) throws Exception {
-        security.httpBasic().disable();
+        security.httpBasic().disable()
+                .cors().and().csrf().disable();
     }
 }
